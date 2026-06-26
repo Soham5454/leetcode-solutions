b00879d51a28535ff3aa7ea5711bb8e7e990144d
Soham <sohamdey5454@gmail.com>
2026-06-26 17:10:38 +0530

    Clean up comments in palindrome_number.py
    Removed comments related to problem statement and performance.

--- palindrome_number.py
+++ palindrome_number.py
@@ -1,8 +1,3 @@
-# 9. Palindrome Number
-# Difficulty: Easy
-# Approach: String Reversal
-# Runtime: 7ms - Beats 63.19%
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         return str(x) == str(x)[::-1]
